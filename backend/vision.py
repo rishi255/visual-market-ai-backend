e10c53021a8145cd52f2a9bb9c0ccee59a69e8aa
Rishikesh Rachchh <rishikeshrachchh@gmail.com>
2021-05-16 01:25:43 +0400

    Fixed object detection, .env credential support

--- backend/vision.py
+++ backend/vision.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 KEYDIR_PATH = os.getenv("KEYDIR_PATH")
+with open(KEYDIR_PATH, "w") as f:
+    f.write(os.getenv("SERVICE_JSON"))
 credentials = service_account.Credentials.from_service_account_file(KEYDIR_PATH)
 client = vision.ImageAnnotatorClient(credentials=credentials)
 
@@ -24,15 +26,11 @@ def detect_objects(image_path: str):
     image = vision.Image(content=content)
     objects = client.object_localization(image=image).localized_object_annotations
 
-    print(type(objects))
-    print(f"Number of objects found: {len(objects)}")
-    for object_ in objects:
-        print(f"\n{object_.name} (confidence: {object_.score})")
-        print("Normalized bounding polygon vertices: ")
-        for vertex in object_.bounding_poly.normalized_vertices:
-            print(f" - ({vertex.x}, {vertex.y})")
-
-    return objects
+    # to prevent a really weird exception
+    if len(objects) == 0:
+        return []
+    else:
+        return [i for i in objects]
 
 
 def detect_logos(image_path: str):
@@ -52,25 +50,32 @@ def detect_logos(image_path: str):
     response = client.logo_detection(image=image)
     logos = response.logo_annotations
 
-    print(type(response))
-    print("Logos:")
-    for logo in logos:
-        print(logo.description)
-
     if response.error.message:
         raise Exception(
             "{}\nFor more info on error messages, check: "
             "https://cloud.google.com/apis/design/errors".format(response.error.message)
         )
 
-    return logos
+    # to prevent a really weird exception
+    if len(logos) == 0:
+        return []
+    else:
+        return [i for i in logos]
 
 
 if __name__ == "__main__":
     print("-" * 100)
-    image_path = "./data/fruits.png"
+    image_path = "backend/data/fruits.png"
     objects = detect_objects(image_path=image_path)
 
+    for object_ in objects:
+        print(f"\n{object_.name} (confidence: {object_.score})")
+        print("Normalized bounding polygon vertices: ")
+        for vertex in object_.bounding_poly.normalized_vertices:
+            print(f" - ({vertex.x}, {vertex.y})")
+
     print("-" * 100)
-    image_path = "./data/logo-collage.png"
+    image_path = "backend/data/logo-collage.png"
     logos = detect_logos(image_path=image_path)
+    for logo in logos:
+        print(logo.description)
